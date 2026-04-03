a6a37757814424e04a17639c02774401d36cd36d
Yaghyapan <dyaghyapan@gmail.com>
2026-04-03 22:32:56 +0530

    Data validation complete

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,11 @@ TRAIN_FILE__NAME : str = "train.csv"
 TEST_FILE_NAME : str = "test.csv"
 
 '''
+Schema file path
+'''
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
+'''
 Data Ingestion related constant start with Data_Ingestion VAR NAME
 '''
 
@@ -23,3 +28,13 @@ DATA_INGESTION_DIR_NAME : str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR : str = "feature_store"
 DATA_INGESTION_INGESTED_DIR : str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO : float = 0.2
+
+'''
+Data Validation related constant start eith Data_Validation VAR name
+'''
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_VALID_DIR:str = "validated"
+DATA_VALIDATION_INVALID_DIR:str ="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str ="report.yaml"
